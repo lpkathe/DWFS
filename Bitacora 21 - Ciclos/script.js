4245d139311892f2cab4ab4ea50b718f98d7e206
Katerine Martinez <lp.kathe@gmail.com>
2020-06-16 18:30:54 -0500

    ciclos

--- Bitacora 21 - Ciclos/script.js
+++ Bitacora 21 - Ciclos/script.js
@@ -1,4 +1,17 @@
-function f {
-for (i=0; i<10; i++)
-document.write ["Conteo"+i];
+function f(a) {
+    for (i=1; i<11; i++) {
+        var li = document.createElement("LI");                 // Create a <li> node
+        var liName = document.createTextNode("Item " + i);     // Create a text node
+        li.appendChild(liName);                              // Append the text to <li>
+        document.getElementById("myListFor").appendChild(li);     // Append <li> to <ul> with id="myListFor"
+    }
+}
+
+function w(a) {
+    for (i=1; i<11; i++) {
+        var li = document.createElement("LI");                 // Create a <li> node
+        var liName = document.createTextNode("Item " + i);     // Create a text node
+        li.appendChild(liName);                              // Append the text to <li>
+        document.getElementById("myListWhile").appendChild(li);     // Append <li> to <ul> with id="myListFor"
+    }
 }
