3724abe568adfb8b63a6ec2de5cdb6d2cd10c49c
Katerine Martinez <lp.kathe@gmail.com>
2020-06-17 19:56:22 -0500

    Edades

--- Bitacora 21 - Challenge/script.js
+++ Bitacora 21 - Challenge/script.js
@@ -2,7 +2,7 @@ var suma = 0;
 var contador = 1;
 
 while (contador <=5) {
-    var numero = prompt("Ingrese un número "+contador+" de 5")
+    let numero = prompt("Ingrese un número "+contador+" de 5")
     
     if (!isNaN(numero) && numero != "") {
         suma += parseInt(numero);
@@ -16,3 +16,17 @@ alert ("La suma de los números es: "+suma);
 var promedio = suma / 5;
 
 alert ("El promedio es: "+promedio);
+
+
+var adultos = 0
+var edad = 1
+
+while (edad != 0) {
+    edad = prompt("Ingrese su edad")
+
+    if (edad >= 18) {
+        adultos++;
+    }
+};
+
+alert ("La cantidad de personas adultas son: "+adultos);
