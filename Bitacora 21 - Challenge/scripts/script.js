6845ca7e0b88f3888662d11524dbb8f62c6473be
Katerine Martinez <lp.kathe@gmail.com>
2020-06-22 01:22:44 -0500

    Funcion numeros a letras.

--- Bitacora 21 - Challenge/scripts/script.js
+++ Bitacora 21 - Challenge/scripts/script.js
@@ -148,3 +148,52 @@ function paresImpares() {
         }
     }
 }
+
+function numerosLetras() {
+
+    let numero = solicitarNumero("Ingrese un número del 1 al 10 para conocer su equivalente en letras");
+    let letra = "";
+
+    switch (numero) {
+        case 1:
+            letra = "'a'";
+            break;
+        case 2:
+            letra = "'b'";
+            break;
+        case 3:
+            letra = "'c'";
+            break;
+        case 4:
+            letra = "'d'";
+            break;
+        case 5:
+            letra = "'e'";
+            break;
+        case 6:
+            letra = "'f'";
+            break;
+        case 7:
+            letra = "'g'";
+            break;
+        case 8:
+            letra = "'h'";
+            break;
+        case 9:
+            letra = "'i'";
+            break;
+        case 10:
+            letra = "'j'";
+            break;
+        default:
+            letra = ""
+    }
+
+    if (numero !== null) {
+        if (letra != "") {
+            alert(numero + " es equivalente a la letra " + letra);
+        } else {
+            alert("El número supera el rango permitido")
+        }
+    }
+}
