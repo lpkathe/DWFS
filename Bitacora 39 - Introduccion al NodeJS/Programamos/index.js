290e227a9f5f68eb026636ba08483ab9a0f79843
Katerine Martinez <lp.kathe@gmail.com>
2020-08-13 08:08:55 -0500

    Rename folders

---
+++ Bitacora 39 - Introduccion al NodeJS/Programamos/index.js
@@ -0,0 +1,9 @@
+const hobbies = ["Read", "Walk", "See Tv", "Play video games", "Sleep"];
+
+const iterHobbies = () => {
+hobbies.forEach(element => {
+    console.log(element)
+})
+};
+
+iterHobbies();
