afa77462e32855e56cd3038308b9998c2e1dc24c
Katerine Martinez <lp.kathe@gmail.com>
2020-08-14 15:27:50 -0500

    Bitacora 40 libreria moment

--- Bitacora 40 - NodeJS II/main.js
+++ Bitacora 40 - NodeJS II/main.js
@@ -1,4 +1,4 @@
 const moment = require('moment');
 const today = new Date();
-moment(today).format('MMMM Do YYYY, h:mm:ss a');
-console.log(today);
+const challenge = moment(today).format('MMMM Do YYYY, h:mm:ss a');
+console.log(challenge);
