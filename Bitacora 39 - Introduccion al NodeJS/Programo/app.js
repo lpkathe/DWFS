dc22df83685fef84b3741e6b471e9a2d5fb817d0
Katerine Martinez <lp.kathe@gmail.com>
2020-08-25 14:01:08 -0500

    Bitacora 39 / Ajuste en citado de variables

--- Bitacora 39 - Introduccion al NodeJS/Programo/app.js
+++ Bitacora 39 - Introduccion al NodeJS/Programo/app.js
@@ -6,6 +6,7 @@ console.log(calculator.sumar(45,6));
 console.log(calculator.multiplicar(7,4));
 console.log(calculator.restar(10,4));
 console.log(calculator.dividir(15,3));
+
 /* Destructuring
 const { sumar, restar, multiplicar, dividir } = require('./calculator.js'); //Destructuring
 console.log(sumar(45,6)) */
--- Bitacora 39 - Introduccion al NodeJS/Programo/calculator.js
+++ Bitacora 39 - Introduccion al NodeJS/Programo/calculator.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 function sumar(a,b){
     let suma = a+b;
-    fs.writeFileSync("log.txt", a + " + " + b + " = " + suma + "\n", {flag: "a+"});
+    fs.writeFileSync("log.txt", `${a} + ${b} = ${suma} "\n"`, {flag: "a+"}); 
     return suma
 }
 function restar(a,b){
     let resta = a-b;
-    fs.writeFileSync("log.txt", a + " - " + b + " = " + resta + "\n", {flag: "a+"});
+    fs.writeFileSync("log.txt", `${a} - ${b} = ${resta} "\n"`, {flag: "a+"});
     return resta
 }
 function multiplicar(a,b){
     let multiplicacion = a*b;
-    fs.writeFileSync("log.txt", a + " x " + b + " = " + multiplicacion + "\n", {flag: "a+"});
+    fs.writeFileSync("log.txt", `${a} x ${b} = ${multiplicacion} "\n"`, {flag: "a+"});
     return multiplicacion
 }
 function dividir(a,b){
     let division = a/b;
-    fs.writeFileSync("log.txt", a + " / " + b + " = " + division + "\n", {flag: "a+"});
+    fs.writeFileSync("log.txt", `${a} / ${b} = ${division} "\n"`, {flag: "a+"});
     return division
 }
 module.exports = {
